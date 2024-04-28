425a40cc626c59c2b9738f65e8947428c8ec7011
ungraduateengineer <161126364+ungraduateengineer@users.noreply.github.com>
2024-04-28 22:29:11 +0530

    Update index.js

--- birthday-counter-master/scripts/index.js
+++ birthday-counter-master/scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'April 29, 2023',
+  birthdate: 'April 29, 2024',
   name: 'Mythili❤️'
 };
 
